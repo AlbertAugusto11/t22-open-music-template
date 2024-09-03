48a15ba4ceae25309bbc50cacfb3497049921e5a
Albert Augusto <167814598+AlbertAugusto11@users.noreply.github.com>
2024-09-03 15:38:18 -0300

    FINALIZADO

--- src/scripts/api.js
+++
@@ -1,7 +0,0 @@
-function api(){
-    fetch('https://openmusic-fake-api.onrender.com/api/musics')
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.error('Erro:', error));
-
-}
